058f3aa0656b0844c97782a711eb5ba9238080ec
VirajVaitha123 <44236859+VirajVaitha123@users.noreply.github.com>
2020-05-16 23:43:13 +0100

    Add files via upload

--- Temporal Difference/OpenAI gym Taxi v2 Project/agent.py
+++ Temporal Difference/OpenAI gym Taxi v2 Project/agent.py
@@ -5,7 +5,7 @@ import random
 
 class Agent:
 
-    def __init__(self, nA=6, eps = 1, gamma = 1.0, alpha = 0.01):
+    def __init__(self, nA=6, eps = 0.0001, gamma = 1.0, alpha = 0.01, eps_decay = 0.9999):
         """ Initialize agent.
 
         Params
@@ -16,11 +16,12 @@ class Agent:
         - gamma: discount rate"""
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
-        self.eps = eps/i_episode
+        self.eps = eps                                      ###!!!first mention of eps (and in the top line arguments line 8) 
         self.gamma = gamma
-        self.gamma = alpha
+        self.alpha = alpha
 
     def select_action(self, state):
+
         """ Given the state, select an action.
 
         Params
@@ -31,7 +32,7 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        if random.random() > self.eps/i_episode:
+        if random.random() > self.eps/i_episode:             ###!!!This is the main issue, obviously this won't work here :(
             return np.argmax(self.Q[state])
         else:                     # otherwise, select an action randomly
             return np.random.choice(self.nA)
@@ -49,14 +50,14 @@ class Agent:
         - done: whether the episode is complete (True or False)
         """
         if not done:
-            next_action = select_action(state)
+            next_action = self.select_action(state)             ###!!!If not done,I want the next action. Is it ok practice to reference the select_action function like this?
             current = self.Q[state][action]
             Qsa_next = self.Q[next_state][next_action] if next_state is not None else 0
             target = reward + (self.gamma * Qsa_next)               # construct TD target
             new_value = current + (self.alpha * (target - current)) # get updated value
             self.Q[state][action] = new_value
        
-        if done:
+        if done:                                                ###!!!I don't see why we need the if done statement as we have the if next_state is not None else 0 line. Is it to apply the break?
             next_state=None
             next_action=None
             current = self.Q[state][action]
@@ -64,3 +65,5 @@ class Agent:
             target = reward + (self.gamma * Qsa_next)               # construct TD target
             new_value = current + (self.alpha * (target - current)) # get updated value
             self.Q[state][action] = new_value
+            break                                              ###!!! This break causes a problem, I think I can run without it as it's in the monitor.py?
+            
